44ebfc3076697f4deac8219b3d02b2b7b33c1255
Fagner Oliveira <fagner.soliver@gmail.com>
2021-04-19 23:47:12 -0300

    Atualização de informações de Controller.

--- src/main/java/br/com/fagner/controller/MapeandoRestPerson.java
+++ src/main/java/br/com/fagner/controller/MapeandoRestPerson.java
@@ -4,6 +4,7 @@ import br.com.fagner.model.CamposTabelaPerson;
 import br.com.fagner.services.MapeamentoServicoPerson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,28 +17,29 @@ public class MapeandoRestPerson {
     private MapeamentoServicoPerson servicoPerson;
 
 
-    @RequestMapping(method= RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     public CamposTabelaPerson create(@RequestBody CamposTabelaPerson camposTabelaPerson) throws Exception {
         return servicoPerson.create(camposTabelaPerson);
     }
 
-    @RequestMapping(method= RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping
     public CamposTabelaPerson update(@RequestBody CamposTabelaPerson camposTabelaPerson) throws Exception {
         return servicoPerson.update(camposTabelaPerson);
     }
 
-    @RequestMapping(value="/{id}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping("/{id}")
     public CamposTabelaPerson findById(@PathVariable("id") Long id) throws Exception {
         return servicoPerson.findById(id);
     }
 
-    @RequestMapping(method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public List<CamposTabelaPerson> findAll() throws Exception {
         return servicoPerson.findAll();
     }
 
-    @RequestMapping(value="/{id}", method= RequestMethod.DELETE)
-    public void delete(@PathVariable("id") Long id) throws Exception {
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) throws Exception {
         servicoPerson.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
